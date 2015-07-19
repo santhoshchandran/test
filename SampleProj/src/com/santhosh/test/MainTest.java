21ca8d91a0b67b091e7a3a7cf413f34fc382efd2
Santhosh Chandran <santhosh.chandran6@gmail.com>
2015-07-19 18:26:38 +0530

    Branch2 - Changes message

--- SampleProj/src/com/santhosh/test/MainTest.java
+++ SampleProj/src/com/santhosh/test/MainTest.java
@@ -1,6 +1,7 @@
 package com.santhosh.test;
 public class MainTest {
 public static void main(String args[]) {
-	System.out.println("branch1 - change 5");
+	System.out.println("Master changes");
+	System.out.println("Branch2 changes - 1");
 }
 }
