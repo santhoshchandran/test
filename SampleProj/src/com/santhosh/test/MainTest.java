7c169d9383b71387a33d1c873d2a5b4a339346fe
Santhosh Chandran <santhosh.chandran6@gmail.com>
2015-07-19 18:35:29 +0530

    Adds dummy method

--- SampleProj/src/com/santhosh/test/MainTest.java
+++ SampleProj/src/com/santhosh/test/MainTest.java
@@ -4,4 +4,8 @@ public static void main(String args[]) {
 	System.out.println("Master changes");
 	System.out.println("Branch2 changes - 1");
 }
+public void getdummy()
+{
+	System.out.println("Dummy msg");
+}
 }
